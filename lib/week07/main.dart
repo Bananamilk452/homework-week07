a1f15fb0e5c3dfc65611140f00c3bd8cf7fd5883
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:06:55 +0900

    Updated Changes

--- lib/week07/main.dart
+++ lib/week07/main.dart
@@ -1,1 +1,23 @@
-impo
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:myapp/week05/calendar_scheduler/database/drift_database.dart';
+import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
+
+void main() async {
+  // 플러터 프레임워크가 준비될 때까지 대기
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
+  final database = LocalDatabase(); // 1 데이터베이스 생성
+  
+  GetIt.I.registerSingleton<LocalDatabase>(database);
+  // 3 GetIt에 데이터베이스 변수 주입하기
+
+  runApp(
+    MaterialApp(
+      home: HomeScreen()
+    )
+  );
+}
