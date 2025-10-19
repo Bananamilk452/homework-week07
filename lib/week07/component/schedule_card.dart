1c56d6f4b5f2c21a722d52cdc8247d881039a7b4
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:49:26 +0900

    Updated Changes

--- lib/week07/component/schedule_card.dart
+++ lib/week07/component/schedule_card.dart
@@ -83,3 +83,21 @@ class _Time extends StatelessWidget {
     );
   }
 }
+
+class _Content extends StatelessWidget {
+  final String content; // 1 내용
+
+  const _Content({
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(// 2 최대한 넓게 늘리기
+      child: Text(
+        content,
+      ),
+    );
+  }
+}
