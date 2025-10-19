5d96d28739128191c182d9cb687b320549e47377
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:24:25 +0900

    Updated Changes

--- lib/week07/database/drift_database.dart
+++ lib/week07/database/drift_database.dart
@@ -1,5 +1,6 @@
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:myapp/week07/model/schedule.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'dart:io';
@@ -11,4 +12,11 @@ part 'drift_database.g.dart'; // part 파일 지정
   tables: [
     Schedules,
   ]
-);
+)
+
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+
+  // 1 Code Generation으로 생성할 클래스 상속
+  Stream<List<Schedule>> watchSchedules
+}
