912ed756bb33da184fe112e893cd6b0d68c4fa92
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:54:25 +0900

    Updated Changes

--- lib/week07/screen/home_screen.dart
+++ lib/week07/screen/home_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:myapp/week05/calendar_scheduler/component/main_calendar.dart';
-import 'package:myapp/week05/calendar_scheduler/component/schedule_bottom_sheet.dart';
-import 'package:myapp/week05/calendar_scheduler/component/schedule_card.dart';
-import 'package:myapp/week05/calendar_scheduler/component/today_banner.dart';
-import 'package:myapp/week05/calendar_scheduler/const/colors.dart';
-import 'package:myapp/week05/calendar_scheduler/database/drift_database.dart';
+import 'package:myapp/week07/calendar_scheduler/component/main_calendar.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:myapp/week07/calendar_scheduler/component/schedule_card.dart';
+import 'package:myapp/week07/calendar_scheduler/component/today_banner.dart';
+import 'package:myapp/week07/calendar_scheduler/const/colors.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
 
 class HomeScreen extends StatefulWidget {
   // 1 StatelessWidget에서 StatefulWidget으로 전환
