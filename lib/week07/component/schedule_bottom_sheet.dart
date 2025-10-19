af954c5c1659779094c31a6b5034952d602a086d
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:32:56 +0900

    Updated Changes

--- lib/week07/component/schedule_bottom_sheet.dart
+++ lib/week07/component/schedule_bottom_sheet.dart
@@ -1,3 +1,8 @@
+import 'package:drift/drift.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:myapp/week07/database/drift_database.dart';
+
 class ScheduleBottomSheet extends StatefulWidget {
   final DateTime selectedDate;
 
@@ -5,4 +10,38 @@ class ScheduleBottomSheet extends StatefulWidget {
     required this.selectedDate,
     Key? key
   }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ScheduleBottomSheetState();
+}
+
+class _ScheduleBottomSheetState extends State<ScheduleBottomSheet> {
+  final GlobalKey<FormState> formKey = GlobalKey(); // 폼 key 생성
+
+  int? startTime;   // 시작 시간 저장 변수
+  int? endTime;     // 종료 시간 저장 변수
+  String? content;  // 일정 내용 저장 변수
+
+  @override
+  Widget build(BuildContext context) {
+    // 1 키보드 높이 가져오기
+    final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
+    void onSavePressed() async {
+      if (formKey.currentState!.validate()) {
+        formKey.currentState!.save();
+
+        await GetIt.I<LocalDatabase>().createSchedule(
+          SchedulesCompanion(
+            startTime: Value(startTime!),
+            endTime: Value(endTime!),
+            content: Value(content!),
+            date: Value(widget.selectedDate),
+          )
+        );
+
+        Navigator.of(context).pop();
+      }
+    }
+  }
 }
