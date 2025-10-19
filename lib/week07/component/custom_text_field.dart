e6acccc679a80ef4556419df9ea43f3fc0664d74
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:10:25 +0900

    Updated Changes

---
+++ lib/week07/component/custom_text_field.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final bool isTime;
+  final FormFieldSetter<String> onSaved;
+  final FormFieldValidator<String> validator;
+
+  const CustomTextField({
+    required this.label,
+    required this.isTime,
+    required this.onSaved,
+    required this.validator
+  })
+}
