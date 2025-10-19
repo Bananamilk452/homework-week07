dfc0514be69a3d3d003c97d5443f3362cb3ede5e
Joonsu Yoon <bananamilk452@gmail.com>
2025-10-19 09:07:25 +0900

    Updated Changes

--- lib/week07/main.dart
+++ lib/week07/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:myapp/week05/calendar_scheduler/database/drift_database.dart';
-import 'package:myapp/week05/calendar_scheduler/screen/home_screen.dart';
+import 'package:myapp/week07/calendar_scheduler/database/drift_database.dart';
+import 'package:myapp/week07/calendar_scheduler/screen/home_screen.dart';
 
 void main() async {
   // 플러터 프레임워크가 준비될 때까지 대기
